df5f74cbe049471a9740a47269b6c393a56a634d
Nestor <nez.prieto@gmail.com>
2021-04-07 14:07:54 +0100

    Added setup files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### ELONES
 
-An implementation of the ELO algorithm by Nestor P. used in the Claim_diff project. 
+An implementation of the Elo algorithm by Nestor P. used in the Claim_diff project. 
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='elones',
+    packages=find_packages(include=['elones']),
+    version='0.1',
+    description='An implementation of the Elo algorithm',
+    author='Nestor Prieto',
+    author_email='your.email@domain.com',
+    url='https://github.com/nestorp/claim-diff_elones',
+    download_url='https://github.com/nestorp/claim-diff_elones/archive/v_01.tar.gz',
+    license='MIT',
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
